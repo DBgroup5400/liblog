665ae34894d5325d3a0fb88f4de85a21c748b379
MonsieurPotato <mr_kken_91th@yahoo.co.jp>
2015-12-02 00:02:26 +0900

    ver 1 changed liblog.php README.md

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # liblog
 メニュー履歴ライブラリ
+
+# need "libdb.php"
+
+# How to use
+Create object of MenuLog Class.
+	ex) $log = new MenuLog( host, user, pass, user_id );
+
+Use ResistMenuLog to resist menu log to database.
+The method returns true if the method can resist log to database.
+	ex) $decision =  $log->ResistMenuLog( user_id, log_list );
+
+Use GetMenuLog to get menu log from database
+The method returns array that like log_list if the method can get log from database.
+	ex) $menulog = $
--- liblog.php
+++ liblog.php
@@ -8,11 +8,11 @@ class MenuLog extends db{
   /* end of protected propaties */
 
   /* constructor */
-  public function __construct( $__host, $__user, $__passwd ){
-    $kind = array( 0 => "main",
+  public function __construct( $__host, $__user, $__passwd, $__uid ){
+    $this->kind = array( 0 => "main",
                    1 => "dish",
                    2 => "sub" );
-    $kind = array( 0 => "Mon",
+    $this->week = array( 0 => "Mon",
                    1 => "Tue",
                    2 => "Wed",
                    3 => "Thu",
@@ -20,13 +20,71 @@ class MenuLog extends db{
                    5 => "Sat",
                    6 => "Sun", );
     parent::__construct( $__host, $__user, $__passwd );
+    $query = "CREATE TABLE UM".$__uid." ( Menu_Name text, Kind_Name varchar(10), Date DATE );";
+    $result = $this->_db_throw_query( "Users_Geo", $query );
   }
   /* end of constructor */
 
   /* public method */
-  /* resist menu log to db */
-  public function ResistMenuLog( $_uid ,$_log ){
-    
+  /* method that resist menu log to db */
+  public function ResistMenuLog( $_uid, $_log ){
+    $query = "TRUNCATE TABLE UM".$_uid.";";
+    $result = $this->_db_throw_query( "Users_Geo", $query );
+
+    $query = "";
+    $tmp = "INSERT INTO UM".$_uid." VALUES ( '";
+    for( $i = 0; $i < 7; $i++ ){
+      for( $j = 0; $j < 3; $j++ ){
+        if( $j == 2 ){
+            if( strpos( $_log[$this->week[$i]][$this->kind[$j]], '|' ) !== false ){
+              $div = explode( '|', $_log[$this->week[$i]][$this->kind[$j]] );
+              $size = count( $div );
+              for( $k = 0; $k < $size; $k++ ){
+                $query = $query.$tmp.$div[$k]."', '".$this->kind[$j]."', ( NOW() + INTERVAL ".$i." DAY ) );";
+              }
+              break;
+            }
+        }
+        $query = $query.$tmp.$_log[$this->week[$i]][$this->kind[$j]]."', '".$this->kind[$j]."', ( NOW() + INTERVAL ".$i." DAY ) );";
+      }
+    }
+    $this->_db_select( "Users_Geo" );
+    $result = mysqli_multi_query($this->_connection, $query);
+    if( !$result ){
+      print( "Quely Failed.\n".mysqli_error( $this->_connection ) );
+      return false;
+    }
+    do{
+      mysqli_store_result( $this->_connection );
+    } while( mysqli_next_result( $this->_connection ) );
+
+    return true;
+  }
+  /* method that get menu log */
+  public function GetMenuLog( $_uid ){
+    $days = 0;
+    $back_date = NULL;
+    $back_kind = NULL;
+    $return = array();
+    $query = "SELECT * from UM".$_uid.";";
+
+    $result = $this->_db_throw_query( "Users_Geo", $query );
+    if( !$result ){
+      print( "Quely Failed.\n".mysqli_error( $this->_connection ) );
+      return NULL;
+    }
+
+    while( ( $record = mysqli_fetch_assoc( $result ) ) != NULL ){
+      if( $back_date != NULL && strcmp( $record["Date"], $back_date ) != 0 )
+        $days++;
+      if( $record["Kind_Name"] == $back_kind )
+        $return[$this->week[$days]][$back_kind] = $return[$this->week[$days]][$back_kind]."|".$record["Menu_Name"];
+      else
+        $return[$this->week[$days]][$record["Kind_Name"]] = $record["Menu_Name"];
+      $back_date = $record["Date"];
+      $back_kind = $record["Kind_Name"];
+    }
+    return $return;
   }
   /* end of public method */
 
